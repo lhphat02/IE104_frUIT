21e13f75e3f48e179907c128dc98deb3ea6fd91e
Minhquan257 <denonxskt@gmail.com>
2022-10-31 10:23:53 +0700

    add navbar, footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
+import React from 'react';
+
 import '../styles/globals.css'
+import {Navbar, Footer} from './index';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return 
+  (
+    <div>
+      <Navbar/>
+      <Component {...pageProps} />
+      <Footer/>
+    </div>
+  )
 }
 
 export default MyApp
