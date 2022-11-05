dd7f998b56492658c4804fa3f977ee7aabd42fd7
phatluu <lhphat.02@gmail.com>
2022-11-05 16:12:05 +0700

    add navbar  + footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Navbar/>
+      <Component {...pageProps} />
+      <Footer/>
+    </div>
+  )
 }
 
 export default MyApp
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,jsx}',
+    './components/**/*.{js,jsx}',
   ],
   darkMode: 'class',
   theme: {
