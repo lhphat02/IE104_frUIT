2ec011c717b984b39d1d50e67342bba8e089d7b3
phatluu <lhphat.02@gmail.com>
2022-11-12 13:22:07 +0700

    add assets

---
+++ assets/index.js
@@ -0,0 +1,28 @@
+/* eslint-disable import/no-anonymous-default-export */
+import creator from './creator.png';
+import creator1 from './creator1.jpg';
+import creator2 from './creator2.png';
+import creator3 from './creator3.jpg';
+import creator4 from './creator4.jpg';
+import creator5 from './creator5.jpg';
+import logo from './logo.png';
+import discord from './discord.png';
+import instagram from './instagram.png';
+import telegram from './telegram.png';
+import twitter from './twitter.png';
+import facebook from './facebook.png';
+
+export default { 
+    creator, 
+    creator1, 
+    creator2, 
+    creator3, 
+    creator4, 
+    creator5, 
+    logo, 
+    discord, 
+    instagram, 
+    telegram, 
+    twitter, 
+    facebook 
+};
