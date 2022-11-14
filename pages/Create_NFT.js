a994a0b904fe41302f2a2f194160ce879f87be04
phatluu <lhphat.02@gmail.com>
2022-11-14 23:34:29 +0700

    change page route name

--- pages/Create_NFT.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Create_NFT = () => {
-  return (
-    <div>Create_NFT</div>
-  )
-}
-
-export default Create_NFT
---
+++ pages/create.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Input from '../components/Input'
+
+
+const Create_NFT = () => {
+  return (
+    <div className='p-12'>
+      <div>Create_NFT</div>
+      <Input/>
+    </div>
+  )
+}
+
+export default Create_NFT
