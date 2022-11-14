6a5fc8c22184fd68c39da8071bf6938888b85fb1
phatluu <lhphat.02@gmail.com>
2022-11-14 22:48:28 +0700

    fix button onclick prop

--- components/Button.jsx
+++ components/Button.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Button=({classStyles, BtnName, handleClick}) => (
     <button
-    type="button"
-    className={`prim-gradient text-sm minlg:text-lg px-6 py-2 minlg:px-8 font-poppins font-semibold text-white ${classStyles}`}
-    onClick="handleClick">
+        type="button"
+        className={`prim-gradient text-sm minlg:text-lg px-6 py-2 minlg:px-8 font-poppins font-semibold text-white ${classStyles}`}
+        onClick={handleClick}>
     {BtnName}
     </button>
 
