449476fd32c630771c2134ca72b3a8acef0ea541
Tuan Anh <20520878@gm.uit.edu.vn>
2022-11-20 15:04:49 +0700

    update footer dark mode coi cái dark mode xem đc chưa + m chỉnh dùm t cái khung your email với email me với +còn cái responsive t ko hiểu lắm nên để t tìm hiểu thêm rồi sửa sau

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,15 +1,17 @@
 import Image from "next/image";
+import { useTheme } from "next-themes";
 
 import images from "../assets";
 import Button from "./Button";
 
+
 const FooterLink = ({ heading, items }) => (
-  <div className="sm:mx-4 text-black">
+  <div className="sm:mx-4 text-black dark:text-prim-gray-1">
     <h3 className="text-sm font-semibold font-poppins mb-8 ">{heading}</h3>
     {items.map((item, index) => (
       <p
         key={index}
-        className="hover:cursor-pointer font-poppins font-normal my-4 hover:text-prim-black-1"
+        className="hover:cursor-pointer font-poppins font-normal my-4 dark:text-prim-gray-1 dark:hover:text-prim-blue hover:text-prim-pink"
       >
         {item}
       </p>
@@ -17,8 +19,9 @@ const FooterLink = ({ heading, items }) => (
   </div>
 );
 const Footer = () => {
+  const {theme} = useTheme();
   return (
-    <footer className="flex justify-center items-center flex-col border-t bg-white border-prim-gray-1 sm:py-4 py-8 w-full ">
+    <footer className="flex justify-center items-center flex-col border-t bg-white border-prim-gray-1 sm:py-4 py-8 w-full dark:bg-prim-dark dark:border-prim-black-1 ">
       {/* ===================Footer UpperSecTion=================== */}
       <div className="w-full minmd:w-4/5 flex flex-row md:flex-col sm:px-4 px-10">
         {/* ===================Left Column=================== */}
@@ -33,32 +36,35 @@ const Footer = () => {
               alt="logo"
             />
 
-            <p className="font-semibold text-md ml-1 text-black">FrUIT</p>
+            <p className="font-semibold text-md ml-1 text-black dark:text-prim-gray-1">FrUIT</p>
           </div>
 
           {/* ===================Get Email=================== */}
-          <p className="font-poppins font-semibold mt-10 ml-2 text-sm text-black">
+          <span>
+          <p className="flex justify-start font-poppins font-semibold mt-10 ml-0 text-sm text-black dark:text-prim-gray-1">
             Get the latest updates
           </p>
-          <div className="flex justify-center items-start flex-row md:w-full minlg:w-200 w-50 mt-20 bg-white border border-prim-gray-2 rounded-md  ">
+          <div className="flex justify-start items-start flex-row md:w-full minlg:w-50 w-300 mt-2 bg-white border dark:border-prim-black-2 border-prim-gray-2 rounded-md">
             <input
               type="email"
               placeholder="Your Email"
-              className="h-full flex-1 w-full font-normal text-xs minlg:text-lg outline-none pl-2 rounded-md"
+              className="h-7 flex-1 font-normal text-sm minlg:text-lg outline-none pl-2 dark:bg-prim-black-2 rounded-md"
             />
             <div className="flex-initial text-black">
-              <Button btnName="Email Me" classStyles="rounded-md h-7" />
+              
+              <Button btnName="Email Me" classStyles="rounded-md h-7"/>
+                           
             </div>
           </div>
-
+          </span>
           {/* ===================Right Column=================== */}
-          <div className=" flex justify-center items-start ml-80 md:ml-10 md:mt-4 text-xs">
+          <div className=" flex justify-center items-start ml-80 md:ml-6 md:mt-4 text-sm dark:text-prim-gray-1">
             <FooterLink
               heading="FrUIT"
               items={["Explore", "How its work", "Contact Us"]}
             />
           </div>
-          <div className="flex justify-end items-end ml-80 md:ml-10 md:mt-4 text-xs">
+          <div className="flex justify-end items-end ml-80 md:ml-6 md:mt-2 text-sm dark:text-prim-gray-1">
             <FooterLink
               heading="Team 5"
               items={[
@@ -72,10 +78,10 @@ const Footer = () => {
           </div>
         </div>
       </div>
-      <span className="bg-prim-gray-1 w-full">
+      <span className="bg-prim-gray-1 dark:bg-prim-dark w-full md:ml-10">
         <div className="flex flex-row justify-between">
           <div className="mt-1 ml-10">
-            <p className="font-poppins font-semibold text-xs text-black ">
+            <p className="font-poppins font-semibold text-xs text-black dark:text-prim-gray-1 ">
               FrUIT, Inc. All Rights Reserved
             </p>
           </div>
