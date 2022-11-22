8fc3672587f279601f192ae5256b607ed347b2aa
phatluu <lhphat.02@gmail.com>
2022-11-23 01:32:26 +0700

    add context

---
+++ context/ABI.js
@@ -0,0 +1,4 @@
+import marketAbi from '../artifacts/contracts/NFTMarketplace.sol/NFTMarketplace.json';
+
+export const MarketAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+export const MarketAddressABI = marketAbi.abi;
