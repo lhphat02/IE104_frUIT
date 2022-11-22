20a023cd97056e8881c6779e02bf686dec46858b
phatluu <lhphat.02@gmail.com>
2022-11-22 21:44:12 +0700

    config élint

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -8,63 +8,70 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "hardhat/console.sol";
 
 contract NFTMarketplace is ERC721URIStorage {
-  using Counters for Counters.Counter;
-  Counters.Counter private _tokenId;
-  Counters.Counter private _itemSold;
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenId;
+    Counters.Counter private _itemSold;
 
-  uint listingPrice = 0.005 ether;
-  address payable owner;
-
-  mapping(uint => MarketItem) private idToMarketItem;
-
-  struct MarketItem {
-    uint tokenId;
-    address payable seller;
+    uint listingPrice = 0.005 ether;
     address payable owner;
-    uint price;
-    bool sold;
-  }
-  
-  event MarketItemCreated(
-    uint indexed tokenId,
-    address seller,
-    address owner,
-    uint price,
-    bool sold
-  );
 
-  constructor() ERC721("frUIT Tokens","UIT") {
-    owner = payable(msg.sender);
-  }
+    mapping(uint => MarketItem) private idToMarketItem;
 
-  //Update Listing Price
-  function updateListingPrice(uint _listingPrice) public payable {
-    require(owner == msg.sender, "Require market's owner to change listing price !");
-    listingPrice = _listingPrice;
-  }
+    struct MarketItem {
+        uint tokenId;
+        address payable seller;
+        address payable owner;
+        uint price;
+        bool sold;
+    }
 
-  //Check Listing Price
-  function getListingPrice() public view returns (uint) {
-    return listingPrice;
-  }
+    event MarketItemCreated(
+        uint indexed tokenId,
+        address seller,
+        address owner,
+        uint price,
+        bool sold
+    );
 
-  //Mint Token and create market item
-  function createToken(string memory tokenURL, uint inputPrice) public payable returns (uint) {
-    _tokenId.increment();
-    uint newTokenId = _tokenId.current();
+    constructor() ERC721("frUIT Tokens", "UIT") {
+        owner = payable(msg.sender);
+    }
 
-    _mint(msg.sender, newTokenId);
-    _setTokenURI(newTokenId, tokenURL);
-    createMarketItem(newTokenId, inputPrice);
-  }
+    //Update Listing Price
+    function updateListingPrice(uint _listingPrice) public payable {
+        require(
+            owner == msg.sender,
+            "Require market's owner to change listing price !"
+        );
+        listingPrice = _listingPrice;
+    }
 
-  // function createMarketItem(uint tokenId, uint price) private returns () {
-  //   require(price > 0,"Price must be higher than 0")
-  //   require(msg.value == listingPrice,"Must have enough token to pay listing price")
-    
-  //   idToMarketItem[tokenId] = MarketItem(
-      
-  //   )
-  // }
-  
+    //Check Listing Price
+    function getListingPrice() public view returns (uint) {
+        return listingPrice;
+    }
+
+    //Mint Token and create market item
+    function createToken(string memory tokenURL, uint inputPrice)
+        public
+        payable
+        returns (uint)
+    {
+        _tokenId.increment();
+        uint newTokenId = _tokenId.current();
+
+        _mint(msg.sender, newTokenId);
+        _setTokenURI(newTokenId, tokenURL);
+        createMarketItem(newTokenId, inputPrice);
+    }
+
+    function createMarketItem(uint tokenId, uint price) private {
+        require(price > 0, "Price must be higher than 0");
+        require(
+            msg.value == listingPrice,
+            "Must have enough token to pay listing price"
+        );
+
+        idToMarketItem[tokenId] = MarketItem();
+    }
 }
--- pages/create.js
+++ pages/create.js
@@ -1,46 +1,42 @@
 import { useDropzone } from 'react-dropzone';
 
 import Button from '../components/Button';
-import Input from '../components/Input'
+import Input from '../components/Input';
 
-const CreateNFT = () => {
-  return (
-    <div className='p-16'>
-      <div className='flex flex-row items-center'>
-        <h1 className="font-poppins text-3xl font-semibold ml-4"
-        >
-          Create your
-        </h1>
-        <div className='font-poppins font-extrabold text-4xl sm:mb-4 ml-2 font-gradient'
-        >
-          NFT
-        </div>
+const CreateNFT = () => (
+  <div className="p-16">
+    <div className="flex flex-row items-center">
+      <h1 className="font-poppins text-3xl font-semibold ml-4">
+        Create your
+      </h1>
+      <div className="font-poppins font-extrabold text-4xl sm:mb-4 ml-2 font-gradient">
+        NFT
       </div>
-
-      <Input
-          inputType="input"
-          title="Name"
-          placeholder="NFT Name"
-        />
-        <Input
-          inputType="textarea"
-          title="Description"
-          placeholder="Describe your NFT"
-        />
-        <Input
-          inputType="number"
-          title="Price"
-          placeholder="NFT Price"
-        />
-        <div className="flex justify-end mt-10">
-          <Button
-            BtnName="Create NFT"
-            classStyles="rounded-lg text-lg active:scale-110 duration-100"
-            handleClick={() => {}}
-          />
-        </div>
     </div>
-  )
-}
 
-export default CreateNFT
+    <Input
+      inputType="input"
+      title="Name"
+      placeholder="NFT Name"
+    />
+    <Input
+      inputType="textarea"
+      title="Description"
+      placeholder="Describe your NFT"
+    />
+    <Input
+      inputType="number"
+      title="Price"
+      placeholder="NFT Price"
+    />
+    <div className="flex justify-end mt-10">
+      <Button
+        BtnName="Create NFT"
+        classStyles="rounded-lg text-lg active:scale-110 duration-100"
+        handleClick={() => {}}
+      />
+    </div>
+  </div>
+);
+
+export default CreateNFT;
