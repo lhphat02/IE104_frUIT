316f4fb9b5cd8166f6a7b9738286b36ce77a6b91
Khanh192111 <20520900@gm.uit.edu.vn>
2022-12-10 23:47:10 +0700

    K

--- context/ABI.js
+++
@@ -1,5 +0,0 @@
-import ABI from './NFTMarketplace.json';
-// import ABI from '../artifacts/contracts/NFTMarketplace.sol/NFTMarketplace.json';
-
-export const ContractAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
-export const ContractAddressABI = ABI.abi;
--- pages/about-us.js
+++ pages/about-us.js
@@ -2,8 +2,75 @@ import react from "react";
 
 const aboutUS = () => {
     return(
-    <div>
+    <div className="ml-20">
         <p className="text-xl">About US</p>
+        <img src="https://th.bing.com/th/id/OIP.JMc2F4SuS4ECyciP-ZzJgwHaE8?pid=ImgDet&rs=1" alt="Chào mừng đến với nhóm 5"></img>
+        <div>
+            <div>Nhóm 5
+                <p>Tất cả thành viên đều là sinh viên trường Đại học Công nghệ Thông tin ĐHQG TP HCM, thuộc khoa Khoa học và Kỹ thuật Thông tin, lớp Công nghệ Chất lượng cao định hướng Nhật Bản và có cùng định hướng về Web. Trải nghiệm, áp dụng và thực hiện triển khai một Website trên mạng Internet, ứng dụng vào thực tiễn là cơ hội để nhóm có thể tự mình mày mò tìm hiểu, khai phá năng lực bản thân trong suốt quá trình vừa học vừa xây dựng web.</p>
+            </div>
+        </div>
+        <div>
+            <div>
+                <img src="" alt="Nguyễn Văn Chọn"></img>
+                <div>Nguyễn Văn Chọn
+                    <p>Vị trí</p>
+                    <p>
+                        tự sự
+                    </p>
+                </div>
+                <img src="" alt=""></img>
+                <img src="" alt=""></img>
+                <img src="" alt=""></img>
+            </div>
+            <div>
+                <img src="" alt="Lưu Huỳnh Phát"></img>
+                <div>Lưu Huỳnh Phát
+                    <p>Vị trí</p>
+                    <p>
+                        tự sự
+                    </p>
+                </div>
+                <img src="" alt=""></img>
+                <img src="" alt=""></img>
+                <img src="" alt=""></img>
+            </div>
+            <div>
+                <img src="" alt="Nguyễn Minh Quân"></img>
+                <div>Nguyễn Minh Quân
+                    <p>Vị trí</p>
+                    <p>
+                        tự sự
+                    </p>
+                </div>
+                <img src="" alt=""></img>
+                <img src="" alt=""></img>
+                <img src="" alt=""></img>
+            </div>
+                <img src="" alt="Hoàng Tuấn Anh"></img>
+                <div>Hoàng Tuấn Anh
+                <p>Vị trí</p>
+                    <p>
+                        tự sự
+                    </p>
+                </div>
+                <img src="" alt=""></img>
+                <img src="" alt=""></img>
+                <img src="" alt=""></img>
+            <div>
+                <img src="" alt="Nguyễn Ngọc Mai Khanh"></img>
+                <div>
+                    <p>Nguyễn Ngọc Mai Khanh</p>
+                    <p>Vị trí</p>
+                    <p>
+                        tự sự
+                    </p>
+                </div>
+                <img src="" alt=""></img>
+                <img src="" alt=""></img>
+                <img src="" alt=""></img>
+            </div>
+        </div>
     </div>
 )}
 
