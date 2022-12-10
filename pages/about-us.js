9b17705960d9effebd6bd90f7c00138f5cc8b4ee
Khanh192111 <20520900@gm.uit.edu.vn>
2022-12-11 01:22:52 +0700

    update about-us

--- pages/about-us.js
+++ pages/about-us.js
@@ -2,65 +2,83 @@ import react from "react";
 
 const aboutUS = () => {
     return(
-    <div className="ml-20">
-        <p className="text-xl">About US</p>
-        <img src="https://th.bing.com/th/id/OIP.JMc2F4SuS4ECyciP-ZzJgwHaE8?pid=ImgDet&rs=1" alt="Chào mừng đến với nhóm 5"></img>
-        <div>
-            <div>Nhóm 5
-                <p>Tất cả thành viên đều là sinh viên trường Đại học Công nghệ Thông tin ĐHQG TP HCM, thuộc khoa Khoa học và Kỹ thuật Thông tin, lớp Công nghệ Chất lượng cao định hướng Nhật Bản và có cùng định hướng về Web. Trải nghiệm, áp dụng và thực hiện triển khai một Website trên mạng Internet, ứng dụng vào thực tiễn là cơ hội để nhóm có thể tự mình mày mò tìm hiểu, khai phá năng lực bản thân trong suốt quá trình vừa học vừa xây dựng web.</p>
+    <div className="mx-20">
+        <p className="my-10 text-3xl">About US</p>
+        <img className="mr-20"
+                src="https://th.bing.com/th/id/OIP.JMc2F4SuS4ECyciP-ZzJgwHaE8?pid=ImgDet&rs=1"
+                alt="Chào mừng đến với nhóm 5" width="100%"></img>
+        <div className="my-10">
+            <div className="border-solid border-t-2 py-3 text-xl text-justify">NHÓM 5
+                <p className="text-base">Tất cả thành viên đều là sinh viên trường Đại học Công nghệ Thông tin ĐHQG TP HCM, thuộc khoa Khoa học và Kỹ thuật Thông tin, lớp Công nghệ Chất lượng cao định hướng Nhật Bản và có cùng định hướng về Web. Trải nghiệm, áp dụng và thực hiện triển khai một Website trên mạng Internet, ứng dụng vào thực tiễn là cơ hội để nhóm có thể tự mình mày mò tìm hiểu, khai phá năng lực bản thân trong suốt quá trình vừa học vừa xây dựng web.</p>
             </div>
         </div>
-        <div>
-            <div>
-                <img src="" alt="Nguyễn Văn Chọn"></img>
-                <div>Nguyễn Văn Chọn
-                    <p>Vị trí</p>
-                    <p>
-                        tự sự
-                    </p>
-                </div>
-                <img src="" alt=""></img>
-                <img src="" alt=""></img>
-                <img src="" alt=""></img>
-            </div>
-            <div>
-                <img src="" alt="Lưu Huỳnh Phát"></img>
-                <div>Lưu Huỳnh Phát
-                    <p>Vị trí</p>
-                    <p>
-                        tự sự
-                    </p>
-                </div>
-                <img src="" alt=""></img>
-                <img src="" alt=""></img>
-                <img src="" alt=""></img>
-            </div>
-            <div>
-                <img src="" alt="Nguyễn Minh Quân"></img>
-                <div>Nguyễn Minh Quân
-                    <p>Vị trí</p>
-                    <p>
-                        tự sự
-                    </p>
-                </div>
-                <img src="" alt=""></img>
-                <img src="" alt=""></img>
-                <img src="" alt=""></img>
-            </div>
-                <img src="" alt="Hoàng Tuấn Anh"></img>
-                <div>Hoàng Tuấn Anh
-                <p>Vị trí</p>
-                    <p>
-                        tự sự
-                    </p>
-                </div>
-                <img src="" alt=""></img>
-                <img src="" alt=""></img>
-                <img src="" alt=""></img>
-            <div>
-                <img src="" alt="Nguyễn Ngọc Mai Khanh"></img>
+        <div className="text-center grid grid-cols-5 gap-5 rounded-2xl border-t-2 py-5">
+            <div className="flex flex-col justify-center items-center">
+                <img className="rounded-full"
+                    src="https://scontent-hkt1-2.xx.fbcdn.net/v/t39.30808-6/252938250_1469386416780461_5835384178997990174_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=fCywtA8RTeQAX--NhQP&_nc_ht=scontent-hkt1-2.xx&oh=00_AfCXXKbv3M2weyrTfKQk9GeOJyVyA00okVEVkNj2UMfOxw&oe=63995BD5"
+                    alt="Nguyễn Văn Chọn" width="50%"></img>
                 <div>
-                    <p>Nguyễn Ngọc Mai Khanh</p>
+                    <p className="text-xl">Nguyễn Văn Chọn</p>
+                    <p>Vị trí</p>
+                    <p>
+                        tự sự
+                    </p>
+                </div>
+                <img src="" alt=""></img>
+                <img src="" alt=""></img>
+                <img src="" alt=""></img>
+            </div>
+            <div className="flex flex-col justify-center items-center">
+                <img className="rounded-full"
+                    src=""
+                    alt="Lưu Huỳnh Phát"></img>
+                <div>
+                    <p className="text-xl">Lưu Huỳnh Phát</p>
+                    <p>Vị trí</p>
+                    <p>
+                        tự sự
+                    </p>
+                </div>
+                <img src="" alt=""></img>
+                <img src="" alt=""></img>
+                <img src="" alt=""></img>
+            </div>
+            <div className="flex flex-col justify-center items-center">
+                <img className="rounded-full"
+                    src="https://scontent-hkt1-2.xx.fbcdn.net/v/t39.30808-6/272880268_626817418618980_1573065678081878384_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=174925&_nc_ohc=LOGDesRQvYkAX_XJIU_&_nc_ht=scontent-hkt1-2.xx&oh=00_AfAC5B9a4S451Se_im4h0ce373WuuxHzXOdm_y652gZqqw&oe=639A9267"
+                    alt="Nguyễn Minh Quân" width="50%"></img>
+                <div>
+                    <p className="text-xl">Nguyễn Minh Quân</p>
+                    <p>Vị trí</p>
+                    <p>
+                        tự sự
+                    </p>
+                </div>
+                <img src="" alt=""></img>
+                <img src="" alt=""></img>
+                <img src="" alt=""></img>
+            </div>
+            <div className="flex flex-col justify-center items-center">
+                <img className="rounded-full"
+                    src=""
+                    alt="Hoàng Tuấn Anh"></img>
+                <div>
+                    <p className="text-xl">Hoàng Tuấn Anh</p>
+                    <p>Vị trí</p>
+                    <p>
+                        tự sự
+                    </p>
+                </div>
+                <img src="" alt=""></img>
+                <img src="" alt=""></img>
+                <img src="" alt=""></img>
+            </div>
+            <div className="flex flex-col justify-center items-center">
+                <img className="rounded-full"
+                    src="https://scontent-hkt1-2.xx.fbcdn.net/v/t1.6435-9/179992532_128598702625355_4405256090229406103_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=m73sky3Yv9UAX9BQCl-&_nc_ht=scontent-hkt1-2.xx&oh=00_AfCg38-EouikC6MjL-EFWXIikbdUUxpSDgFg9lxSZyraVQ&oe=63BC323C"
+                    alt="Nguyễn Ngọc Mai Khanh" width="50%"></img>
+                <div>
+                    <p className="text-xl">Nguyễn Ngọc Mai Khanh</p>
                     <p>Vị trí</p>
                     <p>
                         tự sự
