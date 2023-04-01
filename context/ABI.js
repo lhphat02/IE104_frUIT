7110961492567cceffed681c57104673803be031
phatluu <lhphat.02@gmail.com>
2023-04-01 15:44:49 +0700

    Change market address

--- context/ABI.js
+++ context/ABI.js
@@ -1,5 +1,5 @@
 import ABI from './NFTMarketplace.json';
 // import ABI from '../artifacts/contracts/NFTMarketplace.sol/NFTMarketplace.json';
 
-export const ContractAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+export const ContractAddress = '0x110f3eb1cDc3F3B1ee0A1ba1d388Ec1Af8774278';
 export const ContractAddressABI = ABI.abi;
