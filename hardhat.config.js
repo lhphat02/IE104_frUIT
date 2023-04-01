a6315e85da3675a5b8fcc0766ab8ad4f5c9a4597
phatluu <lhphat.02@gmail.com>
2023-04-01 15:44:25 +0700

    Change network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,15 @@
 require('@nomiclabs/hardhat-waffle');
 
+const ALCHEMY_API_KEY = 'EUyP2mxP_WIQVM4dvzyeN8YxpYa2JGmA';
+const SEPOLIA_PRIVATE_KEY =
+  '239908345ec889b15c5d2f6a7ce7bb21e5997749705cfcd7b86f66385c362dad';
+
 module.exports = {
   networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY],
+    },
     hardhat: {
       chainId: 1337,
     },
