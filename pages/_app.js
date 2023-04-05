7b85d4e75aa0fa8fa78434a829c162c73f425b27
phatluu <lhphat.02@gmail.com>
2023-04-05 22:39:52 +0700

    Update some SEO tags

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import { ThemeProvider } from 'next-themes';
 import Head from 'next/head';
+import { DefaultSeo, NextSeo } from 'next-seo';
 
 import { ContextProvider } from '../context/Context';
 import Footer from '../components/Footer';
@@ -10,9 +11,27 @@ const MyApp = ({ Component, pageProps }) => (
   <ContextProvider>
     <ThemeProvider attribute="class">
       <Head>
-        <title>frUIT Marketplace</title>
         <link rel="icon" href="/logo.png" />
       </Head>
+      <NextSeo
+        title="frUIT Marketplace, from UIT NFT Marketplace"
+        description="A NFT marketplace for NFTs from UIT"
+      />
+      <DefaultSeo
+        title="frUIT Marketplace, from UIT NFT Marketplace"
+        description="A NFT marketplace for NFTs from UIT"
+        openGraph={{
+          type: 'website',
+          locale: 'en_IE',
+          url: 'https://www.fromuitnft.online/',
+          siteName: 'SiteName',
+        }}
+        twitter={{
+          handle: '@handle',
+          site: '@site',
+          cardType: 'summary_large_image',
+        }}
+      />
       <div className="bg-white dark:bg-prim-dark">
         <Navbar />
         <div className="w-full pt-16">
