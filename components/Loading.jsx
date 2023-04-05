e249caf952ec6304b37daf025233ba5cff57d789
phatluu <lhphat.02@gmail.com>
2023-04-05 22:38:05 +0700

    Update new loading animation

--- components/Loading.jsx
+++ components/Loading.jsx
@@ -1,24 +1,9 @@
-import React from 'react';
-import Image from 'next/image';
-import { useTheme } from 'next-themes';
+import { Spinner } from 'flowbite-react';
 
-import assets from '../assets';
 const Loading = () => {
-  const { theme } = useTheme();
   return (
-    <div className="flex flex-col  justify-center items-center  m-2">
-      <div className=" flex relative  justify-center">
-        <Image
-          src={theme === 'dark' ?  assets.loadingdark :assets.loadinglight }
-          alt=""
-          width={300}
-          height={300}
-          objectFit="contain"
-        />
-        <p className="absolute text-2xl font-semibold text-prim-gray-2 font-poppins bottom-3">
-          Loading. .
-        </p>
-      </div>
+    <div className="my-20 text-center">
+      <Spinner color="pink" aria-label="Pink spinner example" size="xl" />
     </div>
   );
 };
