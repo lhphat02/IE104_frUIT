a7c75f0930ce96a10d96f9c12779c151ed8ee7d1
phatluu <lhphat.02@gmail.com>
2023-04-07 16:48:48 +0700

    Fix position

--- components/CarouselComp.jsx
+++ components/CarouselComp.jsx
@@ -5,7 +5,7 @@ import images from '../assets';
 
 const CarouselComp = () => {
   return (
-    <div className="relative h-56 mb-10 xl:h-96 pc:h-300 minmd:h-500">
+    <div className="relative mb-10 h-44 xl:h-96 pc:h-300 minmd:h-500">
       <div className="absolute z-10 w-full h-full bg-black rounded-lg bg-opacity-40" />
       <Carousel leftControl=" " rightControl=" ">
         <Image src={images.bg5} />
