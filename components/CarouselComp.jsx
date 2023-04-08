8d85f02ee3eff4b6f8b8604ad8a4ee173b7f8121
phatluu <lhphat.02@gmail.com>
2023-04-08 10:56:05 +0700

    Update

--- components/CarouselComp.jsx
+++ components/CarouselComp.jsx
@@ -8,11 +8,11 @@ const CarouselComp = () => {
     <div className="relative mb-10 h-44 xl:h-96 pc:h-300 minmd:h-500">
       <div className="absolute z-10 w-full h-full bg-black rounded-lg bg-opacity-40" />
       <Carousel leftControl=" " rightControl=" ">
-        <Image src={images.bg5} />
-        <Image src={images.bg6} />
-        <Image src={images.bg7} />
-        <Image src={images.bg8} />
-        <Image src={images.bg9} />
+        <Image src={images.bg5} alt="from UIT NFT Banner 1" />
+        <Image src={images.bg6} alt="from UIT NFT Banner 2" />
+        <Image src={images.bg7} alt="from UIT NFT Banner 3" />
+        <Image src={images.bg8} alt="from UIT NFT Banner 4" />
+        <Image src={images.bg9} alt="from UIT NFT Banner 5" />
       </Carousel>
     </div>
   );
